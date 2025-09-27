d390f575f42930646d5ca051a9ca975b88b080ed
helloweeb <wehbello@outlook.com>
2025-09-27 15:56:44 +0800

    Update diy-part1clear.sh

--- diy-part1clear.sh
+++ diy-part1clear.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 
 # 安装额外依赖软件包
